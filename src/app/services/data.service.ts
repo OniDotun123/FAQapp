1251addfeb1776304d9fcad1635bfd2e7d1e0539
onidotun123 <onidotun99@gmail.com>
2017-11-01 01:36:47 -0400

    implement local storage feature

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,7 +6,7 @@ export class DataService {
 questions:Question[];
 
   constructor() {
-   this.questions = [
+   /*this.questions = [
       {
         text:"what is your name?",
         answer:"my name is dotun",
@@ -22,24 +22,53 @@ questions:Question[];
         answer:"my fav language is JS",
         hide:true
       }
-    ];
+    ];*/
   }
 
-  getQuestions(){
-    return this.questions;
-  };
 
+
+//Get Question from LS
+  getQuestions(){
+    if(localStorage.getItem('questions') === null){
+      this.questions = [];
+    }else{
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
+    return this.questions;
+  }
+
+//Add Question to local Storage
   addQuestion(question:Question){
       this.questions.unshift(question);
-  };
 
-  removeQuestion(question:Question){
-    for(let i=0;i < this.questions.length;i++){
-      if(question == this.questions[i]){
-        this.questions.splice(i,1);
+      //init local var
+      let questions;
+
+      if(localStorage.getItem('questions') === null){
+        questions = [];
+        //push new question
+        questions.unshift(question);
+        // Set new array to local storage
+        localStorage.setItem('questions', JSON.stringify(questions));
+      }else{
+        questions = JSON.parse(localStorage.getItem('questions'));
+       //Add new question
+        questions.unshift(question);
+
+        localStorage.setItem('questions',JSON.stringify(questions));
+
+
       }
     }
 
-  }
+    //Remove question from local Storage
+    removeQuestion(question:Question){
+      for(let i=0;i < this.questions.length;i++){
+        if(question == this.questions[i]){
+          this.questions.splice(i,1);
+        }
+      }
 
-}
+    }
+
+  }
