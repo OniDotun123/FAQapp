0e94a244e8ab43ad78268985b23f4769264c65fd
onidotun123 <onidotun99@gmail.com>
2017-11-01 00:10:16 -0400

    create questions list component, question component, and services for data

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Question} from '../models/question';
+
+@Injectable()
+export class DataService {
+questions:Question[];
+
+  constructor() {
+    this.questions = [
+      {
+        text:"what is your name?",
+        answer:"my name is dotun",
+        hide:true
+      },
+      {
+        text:"what is your favorite color?",
+        answer:"my fav color is blue",
+        hide:true
+      },
+      {
+        text:"what is your favorite language?",
+        answer:"my fav language is JS",
+        hide:true
+      },
+    ];
+  }
+
+  getQuestions(){
+    return this.questions;
+  };
+
+}
